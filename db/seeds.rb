ba7579f73c1885e62d44a1478c435593341822cd
LorenzTroll <tetrix.t@googlemail.com>
2020-05-26 18:28:04 +0200

    cleaned the seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-puts "Cleaning database..."
-Bike.destroy_all
-
-puts "Creating cargo bikes..."
-omnium0 = { model: "Omnium", bike_type: "Cargo", capacity: "100 kg" }
-omnium1 =  { name: "Omnium", address: "Mini-Max", capacity: "75 kg"}
-
-[ omnium0, omnium1 ].each do |attributes|
-  bike = Bike.create!(attributes)
-  puts "Created #{bike.name}"
-end
-puts "Finished!"
