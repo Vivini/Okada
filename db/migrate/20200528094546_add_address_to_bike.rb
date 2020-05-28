507218fe7c237f09578ba8a2d7462838ab48d5fa
Holly McMahon <hmcmahon25@gmail.com>
2020-05-28 11:56:51 +0200

    migrated db to include bike price, address & user img

---
+++ db/migrate/20200528094546_add_address_to_bike.rb
@@ -0,0 +1,5 @@
+class AddAddressToBike < ActiveRecord::Migration[6.0]
+  def change
+    add_column :bikes, :address, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_155102) do
+ActiveRecord::Schema.define(version: 2020_05_28_094546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2020_05_26_155102) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "price"
+    t.string "img_url"
+    t.string "address"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
@@ -47,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_155102) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "user_name"
+    t.string "img_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
