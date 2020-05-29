1b67dadc8c0ee4e23b9d3af963d494c205483f75
samuel atobatele <swumatob@yahoo.co.uk>
2020-05-29 11:37:09 +0100

    Feature completed

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,9 +1,10 @@
 class Bike < ApplicationRecord
   belongs_to :user
+  has_many_attached :photos
   validates :model, presence: true
   validates :bike_type, presence: true
   validates :capacity, presence: true
   validates :price, presence: true
   validates :address, presence: true
-  validates :img_url, presence: true
+  #validates :img_url, presence: true
 end
